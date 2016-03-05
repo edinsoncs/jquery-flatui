92ad512cf8c6a62239ed0cf939f4f9e6d27fc36c
Edinson <jean_clude_dortosms@hotmail.com>
2016-03-05 15:22:36 -0300

    in fixes

--- jquery.flat.js
+++ jquery.flat.js
@@ -4,18 +4,13 @@
 $.fn.flat = function(color, a){
 	var recept = color;
 	var receptIn = a;
-	function colorsFlats(hex) {
-		var n = hex;
-		var s = n;
-		console.log(s);
-	}
-	function inColor() {
-		colorsFlats({
-			'Colors': [
-						'#E0E4CC','#D6DAC2','#7BB0A6','#92F22A',
+	var shadows;
+	
+	function inColor(select) {
+		var colors = ['#E0E4CC','#D6DAC2','#7BB0A6','#92F22A',
 						'#64DDBB','#7CEECE','#8F6F40','#6F532A','#523D1F','#A0B58D',
 						'#A19C69','#FD5B03','#63393E','#3C3741','#8C7E51','#54573A',
-						'#54573A','#8C7E51','#54573A','#F04903','#FF7416','#F29B34',
+						'#8C7E51','#54573A','#F04903','#FF7416','#F29B34',
 						'#D33257','#3D8EB9','#71BA51','#FEC606','#E75926','#EB6361',
 						'#EBBD63','#6C8784','#45362E','#87766C','#25373D','#CF000F',
 						'#E3000E','#E6DCDB','#D2D7D3','#E7E7E7','#282830','#BADA55',
@@ -24,14 +19,23 @@ $.fn.flat = function(color, a){
 						'#FACA9B','#FDD09F','#E7DF86','#C0BA78','#AAB69B','#9E906E',
 						'#9684A3','#8870FF','#888888','#897FBA','#8870FF','#2C82C9',
 						'#2CC990','#EEE657','#FCB941','#FC6042','#3E4651','#F1654C',
-						'#00B5B5','#D4D4D4'
-					  ]
-		});
+						'#00B5B5','#D4D4D4'];
+		shadows = ['0 3px #C3C7AE', '0 4px red'];
+		var inFind = select;
+		if(inFind == 'flatuno'){
+			var x =	colors[0];
+			var t = shadows[0];
+			return x;
+		}
+
 	}
+
 	inColor();
-	if(recept == 'red' || receptIn == 'a') {
+
+	if(recept == 'flatuno' || receptIn == 'a') {
 		this.css({
-			'background':'red',
+			'background': inColor('flatuno'),
+			'box-shadow': shadows[0],
 			'color':'white',
 			'padding':'0.8em 1.3em',
 			'text-decoration':'none',
